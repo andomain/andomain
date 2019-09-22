886973f341323fcdee085805d66315a81048e580
Sam Anderson <sam@andomain.co.uk>
2019-09-22 20:33:21 +0100

    Layout components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,75 +1,59 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { useStaticQuery } from 'gatsby';
+import styled, { ThemeProvider } from 'styled-components';
 
-import { rhythm, scale } from "../utils/typography"
+import Header from './header';
+import SEO from "./seo"
 
-class Layout extends React.Component {
-  render() {
-    const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    let header
+import { rhythm } from "../utils/typography"
+import '../styles/global.scss';
 
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
+const PageContainer = styled.div`
+    marginLeft: auto;
+    marginRight: auto;
+    maxWidth: rhythm(24);
+    padding: ${ rhythm(1.5)} ${rhythm(3 / 4)};
+`;
+
+const theme = {
+    main: '#252525',
+    highlight: '#ff0000',
+    link: '#af0404',
+    neutral: '#414141',
+}
+
+const Layout = ({ children, pageTitle }) => {
+    const { site } = useStaticQuery(
+        graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
+    );
+
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
-        <header>{header}</header>
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
+
+        <ThemeProvider theme={theme}>
+            <>
+                <SEO pageTitle={pageTitle} {...site.siteMetadata} />
+                <PageContainer>
+                    <Header title={site.siteMetadata.title} />
+                    <main>{children}</main>
+                    <footer>
+                        © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+                        <a href="https://www.gatsbyjs.org">Gatsby</a>
+                    </footer>
+                </PageContainer>
+            </>
+        </ThemeProvider>
     )
-  }
 }
 
 export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,15 @@ import { Link, graphql } from "gatsby"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 
 class BlogIndex extends React.Component {
   render() {
     const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
     const posts = data.allMarkdownRemark.edges
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
+      <Layout pageTitle="Blog">
         <Bio />
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
@@ -51,11 +48,6 @@ export default BlogIndex
 
 export const pageQuery = graphql`
   query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
