bd6ba7eddce52a6ce9ec60ed5bb6b083d2d284a6
Sam Anderson <sam@andomain.co.uk>
2019-09-22 21:23:21 +0100

    Improve bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,18 +1,21 @@
-/**
- * Bio component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import Image from "gatsby-image"
+// import Image from "gatsby-image";
+import styled from 'styled-components';
 
 import { rhythm } from "../utils/typography"
 
+const BioContainer = styled.div`
+    display: flex;
+    marginBottom: rhythm(1);
+
+    @media(max-width: ${props => props.theme.mobileBreakpoint}px) {
+        flex-direction: column;
+    }
+`;
+
 const Bio = () => {
-  const data = useStaticQuery(graphql`
+    const data = useStaticQuery(graphql`
     query BioQuery {
       avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
         childImageSharp {
@@ -33,36 +36,27 @@ const Bio = () => {
     }
   `)
 
-  const { author, social } = data.site.siteMetadata
-  return (
-    <div
-      style={{
-        display: `flex`,
-        marginBottom: rhythm(1),
-      }}
-    >
-      <Image
-        fixed={data.avatar.childImageSharp.fixed}
-        alt={author}
-        style={{
-          marginRight: rhythm(1 / 2),
-          marginBottom: 0,
-          minWidth: 50,
-          borderRadius: `100%`,
-        }}
-        imgStyle={{
-          borderRadius: `50%`,
-        }}
-      />
-      <p>
-        Personal website of Sam Anderson, Fullstack web developer specializing in building nice things for nice people.
-        {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
-        </a>
-      </p>
-    </div>
-  )
+    const { author, social } = data.site.siteMetadata
+    return (
+        <BioContainer>
+            {/* <Image
+                fixed={data.avatar.childImageSharp.fixed}
+                alt={author}
+                style={{
+                    marginRight: rhythm(1 / 2),
+                    marginBottom: 0,
+                    minWidth: 50,
+                    borderRadius: `100%`,
+                }}
+                imgStyle={{
+                    borderRadius: `50%`,
+                }}
+            /> */}
+            <p>
+                I'm Sam Anderson, a Fullstack web developer specializing in building nice things for nice people. I'm currently  based in Sheffield working at <a href="https://joipolloi.com/">Joi Polloi</a>.
+            </p>
+        </BioContainer>
+    )
 }
 
 export default Bio
